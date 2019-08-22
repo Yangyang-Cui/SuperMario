3f87183b4b28ea17f9c2761bc39bf233f6df75b9
Yangyang Cui <cuiyang21@gmail.com>
2019-08-22 22:14:52 +0800

    ignore learn.js/html

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
+function loadImage (url) {
+  return new Promise(resolve => {
+    const image = new Image()
+    image.addEventListener('load', () => {
+      resolve(image)
+    })
+    image.src = url
+  })
+}
+
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
+
 context.fillRect(0, 0, 50, 50)
+
+loadImage('../img/tiles.png')
