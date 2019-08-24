06ebdc2820aa89841f0d17ede1c2dfec2b4d6c7d
Yangyang Cui <cuiyang21@gmail.com>
2019-08-24 21:15:01 +0800

    add for loop

--- public/js/main.js
+++ public/js/main.js
@@ -11,4 +11,10 @@ loadImage('../img/tiles.png')
     const sprites = new SpriteSheet(image, 16, 16)
     sprites.define('ground', 0, 0)
     sprites.draw('ground', context, 32, 32)
+    sprites.define('sky', 3, 21)
+    for (let x = 0; x < 25; x++) {
+      for (let y = 0; y < 14; y++) {
+        sprites.draw('sky', context, x * 16, y * 16)
+      }
+    }
   })
