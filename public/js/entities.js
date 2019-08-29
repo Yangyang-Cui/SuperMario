f0bd8d340cd71590cd52851c78f18ce2a08d9ab9
Yangyang Cui <cuiyang21@gmail.com>
2019-08-29 17:36:20 +0800

    video 3 has done 12:35

---
+++ public/js/entities.js
@@ -0,0 +1,19 @@
+import { loadMarioSprite } from './sprites.js'
+import Entity from './Entity.js'
+export function createMario () {
+  return loadMarioSprite()
+    .then((sprite) => {
+      const mario = new Entity()
+      mario.pos.set(64, 180)
+      mario.vel.set(2, -10)
+      mario.update = function updateMario () {
+        this.pos.x += this.vel.x
+        this.pos.y += this.vel.y
+      }
+      mario.draw = function drawMario (context) {
+        sprite.draw('idle', context, this.pos.x, this.pos.y)
+      }
+
+      return mario
+    })
+}
