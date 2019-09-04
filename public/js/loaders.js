6ec896f399986ebfaef5e8075b98beaaafaa3a5f
Yangyang Cui <cuiyang21@gmail.com>
2019-09-05 07:00:45 +0800

    video 5 has done 29:05

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -12,6 +12,20 @@ export function loadImage (url) {
   })
 }
 
+function createTiles (level, backgrounds) {
+  backgrounds.forEach((background) => {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+      for (let x = x1; x < x2; x++) {
+        for (let y = y1; y < y2; y++) {
+          level.tiles.set(x, y, {
+            name: background.tile
+          })
+        }
+      }
+    })
+  })
+}
+
 export async function loadLevel (name) {
   const [levelSpec, backgroundSprites] = await Promise.all([
     window.fetch(`../levels/${name}.json`)
@@ -19,8 +33,9 @@ export async function loadLevel (name) {
     loadBackgroundSprites()
   ])
   const level = new Level()
+  createTiles(level, levelSpec.backgrounds)
   const backgroundLayer = createBackgroundLayer(
-    levelSpec.backgrounds,
+    level,
     backgroundSprites
   )
   level.comp.layers.push(backgroundLayer)
--- public/js/math.js
+++ public/js/math.js
@@ -1,3 +1,32 @@
+export class Matrix {
+  constructor () {
+    this.grid = []
+  }
+
+  set (x, y, value) {
+    if (!this.grid[x]) {
+      this.grid[x] = []
+    }
+    this.grid[x][y] = value
+  }
+
+  get (x, y) {
+    const col = this.grid[x]
+    if (col) {
+      return col[y]
+    }
+    return undefined
+  }
+
+  forEach (callback) {
+    this.grid.forEach((column, x) => {
+      column.forEach((value, y) => {
+        callback(value, x, y)
+      })
+    })
+  }
+}
+
 export class Vec2 {
   constructor (x, y) {
     this.set(x, y)
