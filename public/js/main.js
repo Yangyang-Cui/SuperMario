46eedca2ae84f3d1a5c405e239f973edd08259ef
Yangyang Cui <cuiyang21@gmail.com>
2019-09-09 17:13:51 +0800

    video 6 start like youTube

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import { loadLevel } from './loaders.js'
 import { createMario } from './entities.js'
 import Timer from './Timer.js'
 import { setupKeyboard } from './setupKeyboard.js'
-// import { createCollisionLayer } from './layers.js'
+import { createCollisionLayer } from './layers.js'
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
@@ -13,10 +13,20 @@ Promise.all([
 ]).then(([mario, level]) => {
   mario.pos.set(64, 64)
   level.entities.add(mario)
-  // level.comp.layers.push(createCollisionLayer(level))
+  level.comp.layers.push(createCollisionLayer(level))
 
   const input = setupKeyboard(mario)
   input.listenTo(window)
+
+  const mouseControl = ['mousedown', 'mousemove']
+  mouseControl.forEach((eventName) => {
+    canvas.addEventListener(eventName, (event) => {
+      if (event.buttons === 1) {
+        mario.vel.set(0, 0)
+        mario.pos.set(event.clientX, event.clientY)
+      }
+    })
+  })
 
   const timer = new Timer(1 / 60)
   timer.update = function update (deltaTime) {
