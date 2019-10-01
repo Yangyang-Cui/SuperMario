62a6c878eefff4efc527724f3c602011ffb8a7e1
Yangyang Cui <cuiyang21@gmail.com>
2019-10-01 08:38:35 +0800

    video 9 has learned 10:00

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -13,36 +13,6 @@ export function loadImage (url) {
   })
 }
 
-function createTiles (level, backgrounds) {
-  function applyRange (background, xStart, xLen, yStart, yLen) {
-    const xEnd = xStart + xLen
-    const yEnd = yStart + yLen
-
-    for (let x = xStart; x < xEnd; x++) {
-      for (let y = yStart; y < yEnd; y++) {
-        level.tiles.set(x, y, {
-          name: background.tile,
-          type: background.type
-        })
-      }
-    }
-  }
-  backgrounds.forEach((background) => {
-    background.ranges.forEach((range) => {
-      if (range.length === 4) {
-        const [xStart, xLen, yStart, yLen] = range
-        applyRange(background, xStart, xLen, yStart, yLen)
-      } else if (range.length === 3) {
-        const [xStart, xLen, yStart] = range
-        applyRange(background, xStart, xLen, yStart, 1)
-      } else if (range.length === 2) {
-        const [xStart, yStart] = range
-        applyRange(background, xStart, 1, yStart, 1)
-      }
-    })
-  })
-}
-
 function loadJSON (url) {
   return window.fetch(url).then(r => r.json())
 }
@@ -72,7 +42,7 @@ export async function loadLevel (name) {
   const levelSpec = await loadJSON(`../levels/${name}.json`)
   const backgroundSprites = await loadSpriteSheet(levelSpec.spriteSheet)
   const level = new Level()
-  createTiles(level, levelSpec.backgrounds)
+  createTiles(level, levelSpec.backgrounds, levelSpec.patterns)
   const backgroundLayer = createBackgroundLayer(
     level,
     backgroundSprites
@@ -82,4 +52,41 @@ export async function loadLevel (name) {
   level.comp.layers.push(spriteLayer)
 
   return level
+}
+
+function createTiles (level, backgrounds, patterns, offsetX = 0, offsetY = 0) {
+  function applyRange (background, xStart, xLen, yStart, yLen) {
+    const xEnd = xStart + xLen
+    const yEnd = yStart + yLen
+
+    for (let x = xStart; x < xEnd; x++) {
+      for (let y = yStart; y < yEnd; y++) {
+        const deriveX = x + offsetX
+        const deriveY = y + offsetY
+        if (background.pattern) {
+          const backgrounds = patterns[background.pattern].backgrounds
+          createTiles(level, backgrounds, patterns, deriveX, deriveY)
+        } else {
+          level.tiles.set(deriveX, deriveY, {
+            name: background.tile,
+            type: background.type
+          })
+        }
+      }
+    }
+  }
+  backgrounds.forEach((background) => {
+    background.ranges.forEach((range) => {
+      if (range.length === 4) {
+        const [xStart, xLen, yStart, yLen] = range
+        applyRange(background, xStart, xLen, yStart, yLen)
+      } else if (range.length === 3) {
+        const [xStart, xLen, yStart] = range
+        applyRange(background, xStart, xLen, yStart, 1)
+      } else if (range.length === 2) {
+        const [xStart, yStart] = range
+        applyRange(background, xStart, 1, yStart, 1)
+      }
+    })
+  })
 }
